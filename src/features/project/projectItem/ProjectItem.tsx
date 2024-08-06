59bd481baa1fc8a9b669c490c9a6ca6ae71dbdf5
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-06 22:29:13 +0200

    Display duration

--- src/features/project/projectItem/ProjectItem.tsx
+++ src/features/project/projectItem/ProjectItem.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useEffect, useState } from "react";
 import { Button } from "../../../components/button/Button";
 import { Toolbar } from "../../../components/toolbar/Toolbar";
+import { DateTime } from "../../../core/services/date/DateTime";
+import { Duration } from "../../../core/services/date/Duration";
 import { texts } from "../../../hooks/useTranslation/texts";
 import { useTranslation } from "../../../hooks/useTranslation/useTranslation";
 import { DeleteIcon } from "../../../icons/DeleteIcon";
@@ -9,6 +12,10 @@ import { IProjectItemProps } from "./IProjectItemProps";
 import styles from "./ProjectItem.module.scss";
 
 export const ProjectItem: React.FC<IProjectItemProps> = (props) => {
+  const [duration, setDuration] = useState<Duration | undefined>(undefined);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | undefined>(
+    undefined
+  );
   const { t } = useTranslation();
 
   const onDelete = () => {
@@ -21,11 +28,50 @@ export const ProjectItem: React.FC<IProjectItemProps> = (props) => {
     }
   };
 
-  const onStop = () => props.onStop?.(props.project);
+  const onStop = () => {
+    props.onStop?.(props.project);
+    stopTimer();
+  };
 
   const onStart = () => props.onStart?.(props.project);
 
   const isRunning = ProjectInfo.hasRunningTask(props.project);
+
+  const calcDuration = useCallback(() => {
+    const task = ProjectInfo.findRunningTask(props.project);
+    let duration: Duration | undefined = undefined;
+    if (task) {
+      if (task.stoppedAt) {
+        duration = DateTime.subtract(task.startedAt, task.stoppedAt);
+        setDuration(duration);
+      } else {
+        duration = DateTime.subtract(task.startedAt, new Date());
+        setDuration(duration);
+      }
+    } else {
+      duration = undefined;
+      setDuration(undefined);
+    }
+    return duration;
+  }, [props.project]);
+
+  const startTimer = useCallback(() => {
+    const timeoutId = setTimeout(() => {
+      calcDuration();
+      startTimer();
+    }, 1000);
+    setTimeoutId(timeoutId);
+  }, [calcDuration]);
+
+  const stopTimer = useCallback(() => clearTimeout(timeoutId), [timeoutId]);
+
+  useEffect(() => {
+    if (isRunning) {
+      startTimer();
+    } else {
+      setDuration(undefined);
+    }
+  }, [isRunning, startTimer]);
 
   return (
     <div className={styles.projectItem}>
@@ -46,6 +92,13 @@ export const ProjectItem: React.FC<IProjectItemProps> = (props) => {
             {t(texts.projectItem.start)}
           </Button>
         )}
+        <div>
+          {isRunning && (
+            <>
+              {`${duration?.hours}:${duration?.minutes}:${duration?.seconds}`}
+            </>
+          )}
+        </div>
       </Toolbar>
     </div>
   );
